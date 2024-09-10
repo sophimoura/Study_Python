ad91728730dce0cf81741cb447647c24b8f469ec
Sophia Moura <124884847+sophimoura@users.noreply.github.com>
2024-09-09 23:08:16 -0300

    Create main.py

---
+++ CALCULO-DE-AREA/main.py
@@ -0,0 +1,16 @@
+print ('Cálculo da área de um triângulo ')
+base = int(input ("Informe a medida da base:"))
+altura = int(input ("Informe a medida da altura:"))
+área = base*altura/2
+print  ('A área do triângulo é:', área)
+
+print ('Cálculo da área de um quadrado ')
+lado = int(input ('Informe a medida da base:'))
+área = lado**2
+print ("A área do quadrado é:", área)
+
+print ('Cálculo da área de um retângulo ')
+base = int(input ("Informe a medida da base:"))
+altura = int(input ("Informe a medida da altura:"))
+área = base*altura
+print  ('A área do retângulo é:', área)
